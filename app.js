e75377617f1f3723d3ccd6bf6fa09d5f9bc64897
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-01-22 01:13:59 -0300

    atualizaçao do javascript

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ function scrollPage(numero){
     const selectSection = ["aboutMe","myProjects","Me"]
     const positionArray = selectSection[numero]
     document.getElementById(`${positionArray}`).scrollIntoView({behavior:"smooth"});
+    
     if(numero == 2){
         const thanks = document.getElementById("thanks")
         thanks.style.paddingBottom = "150px"
-        thanks.style.fontFamily = "Montserrat, sans-serif"
         thanks.innerHTML = `Obrigado por visitar meu Portfolio!`
     }
 }
+/* Criar uma funçao que muda a cor do fundo enquanto a rolagem da pagina é executada  */
