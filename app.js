9e058e2d35a0964cb77508aa25facc77235f0186
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-01-22 00:17:10 -0300

    funçoes basicas em javascript

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ function scrollPage(numero){
     const selectSection = ["aboutMe","myProjects","Me"]
     const positionArray = selectSection[numero]
     document.getElementById(`${positionArray}`).scrollIntoView({behavior:"smooth"});
-    
+    if(numero == 2){
+        const thanks = document.getElementById("thanks")
+        thanks.style.paddingBottom = "150px"
+        thanks.style.fontFamily = "Montserrat, sans-serif"
+        thanks.innerHTML = `Obrigado por visitar meu Portfolio!`
+    }
 }
